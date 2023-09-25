6028956ad0348fed1fa1a98a0d3fb595798ed086
Arjab Khadka <arjabkhadka93@gmail.com>
2023-09-25 16:09:54 +0545

    hoisting learned

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ let: changable variable
 const: constant / doesn't change
 */
 
-let p = "ram";
+let p = "ram"; //string
 p = "shyam";
 console.log(p);
 
 const per = "ram";
 console.log(per);
 
-const pi = 3.14;
+const pi = 3.14; //number
 console.log(pi);
+
+const isValid = true; //boolean
+console.log(isValid);
+
+//variable hoisting
+
+a = "holababa";
+console.log(a);
+
+var a; //hoisting
+//let a; not hoisting
