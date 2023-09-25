0cad060ca19488f4a2507da436fe072b665e160f
Arjab Khadka <arjabkhadka93@gmail.com>
2023-09-25 16:18:08 +0545

    prompt types

--- index.js
+++ index.js
@@ -25,10 +25,18 @@ console.log(pi);
 const isValid = true; //boolean
 console.log(isValid);
 
-//variable hoisting
+//variable hoisting: refers to hoist the variable to the top as initialization before assigning 
 
 a = "holababa";
 console.log(a);
 
 var a; //hoisting
 //let a; not hoisting
+
+const personName = prompt("What is your name?");
+console.log(personName);
+
+const isAbove18 = confirm("Are you above 18 years of age?");
+console.log(isAbove18);
+
+alert(personName, isAbove18);
