466847dd6befdb175c00b9e2a756f06132020948
Arjab Khadka <arjabkhadka93@gmail.com>
2023-09-25 15:54:32 +0545

    learned es6

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 console.log("Hello from js");
 //REPL(Read, Evaluate, Print, Loop)
+
+//ES5
+var person = "arjab";
+person = "arjab khadka";
+console.log(person);
+
+//ES6
+/*
+let: changable variable
+const: constant / doesn't change
+*/
+
+let p = "ram";
+p = "shyam";
+console.log(p);
+
+const per = "ram";
+console.log(per);
+
+const pi = 3.14;
+console.log(pi);
